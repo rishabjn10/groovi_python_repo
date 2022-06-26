b787a628c5cbb282b4801b524e3df4756ba527ab
Rishabh Jain <65777129+rishabjn10@users.noreply.github.com>
2022-06-26 23:44:26 +0530

    Update RunningSum1D.py

--- RunningSum1D.py
+++ RunningSum1D.py
@@ -1,7 +1,6 @@
-class Solution:
-    def runningSum(self, nums: List[int]) -> List[int]:
-        x=[0]*len(nums)
-        x[0]=nums[0]
-        for i in range(1, len(nums)):
-            x[i]=x[i-1]+nums[i]
-        return x
+def runningSum(self, nums: List[int]) -> List[int]:
+    x=[0]*len(nums)
+    x[0]=nums[0]
+    for i in range(1, len(nums)):
+        x[i]=x[i-1]+nums[i]
+    return x
