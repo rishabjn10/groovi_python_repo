e88a55d3ad68dd73e6ba046019d5201af207c815
Rishabh Jain <65777129+rishabjn10@users.noreply.github.com>
2022-06-26 23:44:05 +0530

    Update PivotIndex.py

--- PivotIndex.py
+++ PivotIndex.py
@@ -1,9 +1,8 @@
-class Solution:
-    def pivotIndex(self, nums: List[int]) -> int:
-        totalsum= sum(nums)
-        leftsum= 0
-        for i in range(len(nums)):
-            if (totalsum-leftsum-nums[i])==leftsum:
-                return i
-            leftsum+=nums[i]
-        return -1
+def pivotIndex(self, nums: List[int]) -> int:
+    totalsum= sum(nums)
+    leftsum= 0
+    for i in range(len(nums)):
+        if (totalsum-leftsum-nums[i])==leftsum:
+            return i
+        leftsum+=nums[i]
+    return -1
